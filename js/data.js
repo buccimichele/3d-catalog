28fb39ac3146829f46db1fcad194865bb2e176dc
bucci <bucci@eurixgroup.com>
2026-07-21 12:04:28 +0200

    new items

--- js/data.js
+++ js/data.js
@@ -42,12 +42,12 @@ const CREATIONS = [
     images: ["images/posterspiderman1.JPG", "images/posterspiderman2.JPG", "images/posterspiderman3.JPG", "images/posterspiderman4.JPG"],
   },
   {
-    title: "Hollow Knight Diorama Lamp 3D",
+    title: "Hollow Knight Lampada Diorama 3D",
     category: "decorativi",
     images: ["images/hollowlamp1.JPG", "images/hollowlamp2.JPG", "images/hollowlamp3.JPG", "images/hollowlamp4.JPG", "images/hollowlamp5.JPG"],
   },
   {
-    title: "Astrobot Controller Stand",
+    title: "Astrobot Porta Controller",
     category: "funzionali",
     images: ["images/astrocontrollerholder1.JPG", "images/astrocontrollerholder2.JPG", "images/astrocontrollerholder3.JPG", "images/astrocontrollerholder4.JPG"],
   },
@@ -85,5 +85,65 @@ const CREATIONS = [
     title: "Portachiavi Trofeo Mondiali Fifa 2026",
     category: "calcio",
     images: ["images/trofeomondialififakeychain1.JPG"],
+  },
+  {
+    title: "Statua Absolute Batman",
+    category: "decorativi",
+    images: ["images/absolutebatmanstatue1.jpg"],
+  },
+  {
+    title: "Statuetta Cardellino",
+    category: "decorativi",
+    images: ["images/cardellino1.png", "images/cardellino2.JPG", "images/cardellino3.JPG", "images/cardellino4.JPG"],
+  },
+  {
+    title: "Statua Crash Bandicoot Low Poly",
+    category: "decorativi",
+    images: ["images/crashlowpoly1.JPG", "images/crashlowpoly2.JPG"],
+  },
+  {
+    title: "Medaglie Pokémon Galar - Spada e Scudo",
+    category: "decorativi",
+    images: ["images/galarmedals1.JPG", "images/galarmedals2.JPG", "images/galarmedals3.JPG"],
+  },
+  {
+    title: "Statua Luffy Gear 5",
+    category: "decorativi",
+    images: ["images/luffystatue1.JPG", "images/luffystatue2.JPG", "images/luffystatue3.JPG"],
+  },
+  {
+    title: "Pappagallo Minecraft",
+    category: "decorativi",
+    images: ["images/minecraftparrot1.JPG", "images/minecraftparrot2.JPG", "images/minecraftparrot3.JPG"],
+  },
+  {
+    title: "Fermalibro Pragmata",
+    category: "fermalibri",
+    images: ["images/pragmatabooknook1.JPG", "images/pragmatabooknook2.JPG", "images/pragmatabooknook3.JPG", "images/pragmatabooknook4.JPG"],
+  },
+  {
+    title: "Fermalibro Kamado Tanjiro | Demon Slayer",
+    category: "fermalibri",
+    images: ["images/tanjirobooknook1.JPG", "images/tanjirobooknook2.JPG"],
+  },
+  {
+    title: "Quadro Thor | Marvel Rivals",
+    category: "quadri",
+    images: ["images/quadrothor1.JPG", "images/quadrothor2.JPG", "images/quadrothor3.JPG", "images/quadrothor4.JPG"],
+  },
+  {
+    title: "Requiem Desert Eagle | Resident Evil 9: Requiem",
+    category: "decorativi",
+    images: ["images/requiem1.JPG", "images/requiem2.JPG", "images/requiem3.JPG"],
+  },
+  {
+    title: "Statua Rudo Surebrec | Gachiakuta",
+    category: "decorativi",
+    images: ["images/rudostatue1.png", "images/rudostatue2.JPG", "images/rudostatue3.JPG", "images/rudostatue4.JPG", "images/rudostatue5.JPG"],
+  },
+  {
+    title: "Trofei Playstation",
+    category: "decorativi",
+    images: ["images/trofeips1.JPG", "images/trofeips2.JPG"],
   },
 ];
